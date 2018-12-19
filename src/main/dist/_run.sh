a5eac10e5eca69185a36e62465b35536f1149572
Marek Tutaj <mtutaj@mcw.edu>
2018-12-19 08:54:28 -0600

    updated dependencies & build.gradle

--- src/main/dist/_run.sh
+++ src/main/dist/_run.sh
@@ -6,9 +6,6 @@ APPNAME=OmimPipeline
 APPDIR=/home/rgddata/pipelines/$APPNAME
 
 cd $APPDIR
-pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml"
-export OMIM_PIPELINE_OPTS="$DB_OPTS $LOG4J_OPTS"
-
-bin/$APPNAME "$@" 2>&1
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
+    -jar lib/${APPNAME}.jar "$@" 2>&1
